d179c3dba1aafb60943bf933ecc3d00d077e3b39
Max Kolyadin <makcyxa-k@yandex.ru>
2017-06-09 14:34:13 +0300

    First commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="nav has-shadow">
+    <div class="container">
+      <div class="nav-left">
+        <a class="nav-item is-tab is-active"><i class="fa fa-home"></i> &nbsp; Home</a>
+        <a class="nav-item is-tab"><i class="fa fa-bolt"></i> &nbsp; Moments</a>
+        <a class="nav-item is-tab"><i class="fa fa-bell-o"></i> &nbsp; Notifications</a>
+        <a class="nav-item is-tab"><i class="fa fa-envelope"></i> &nbsp; Messages</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
